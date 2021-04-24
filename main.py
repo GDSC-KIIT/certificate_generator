27e171d844a989d75f27e3705e57d2753891638e
Uzair <72073401+uzair-ali10@users.noreply.github.com>
2021-04-24 12:41:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import pandas as pd
 import smtplib 
 import string
@@ -11,50 +12,54 @@ from pandas import ExcelFile
 from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 import os
+
+                                                                                            # Selecting desired font and .csv file contaning participants
 df = pd.read_csv('D:\\Dev\\Python\\VS Code\\certificate_generator\\participants\\Book1.csv')
 font = ImageFont.truetype('D:\\Dev\\Python\\VS Code\\certificate_generator\\fonts\\GoogleSans-Bold.ttf',60)
+
+                                                                                               # Itrating over each participant from the .csv file
 for index,j in df.iterrows():
-    img = Image.open('D:\\Dev\\Python\\VS Code\\certificate_generator\\Templates\\new.png')
+    img = Image.open('D:\\Dev\\Python\\VS Code\\certificate_generator\\Templates\\new.png')    # selecting certificate template
     draw = ImageDraw.Draw(img)
-    draw.text(xy=(900,600),text='{}'.format(j['name'].title()),fill=(1,2,3),font=font)
-    img=img.convert('RGB')
-    img.save('pictures/{}.jpg'.format(j['name']))
-    fromaddr = "darkwhite435@gmail.com"
-    password = "Uzair@Saad786"
+    draw.text(xy=(990,680),text='{}'.format(j['name'].title()),fill=(1,2,3),font=font, anchor="mm")         # Enscribing name over template
+    img=img.convert('RGB') 
+    img.save('pictures/{}.pdf'.format(j['name']))                                              # saving the image
+    fromaddr = "your-email-id@gmail.com"                                                       #Specify the E-Mail ID through which you want to send mail
+    password = "YOUR-PASSWORD"                                                                 #Specify password of the E-Mail ID through which you want to send mail
     toaddr = j['Email']
-    # instance of MIMEMultipart
+                                                                                               # instance of MIMEMultipart
     msg = MIMEMultipart()
-    # storing the senders email address
-    msg['From'] = "darkwhite435@gmail.com"
+                                                                                               # storing the senders email address
+    msg['From'] = fromaddr
     msg['To'] = j['Email']
     msg['Subject'] = "Certificate for Partiicipation [DSC-KIIT]"
-    # string to store the body of the mail
+                                                                                               # string to store the body of the mail
     body = "Hey, Thank you for participating in our event attached below is the certificate for your Partiicipation"
-    # attach the body with the msg instance
+                                                                                               # attach the body with the msg instance
     msg.attach(MIMEText(body, 'plain'))
-    # open the file to be sent
-    filename = 'pictures/{}.jpg'.format(j['name'])
+                                                                                               # open the file to be sent
+    filename = 'pictures/{}.pdf'.format(j['name'])
     attachment = open(filename, "rb")
-    # instance of MIMEBase and named as p
+                                                                                               # instance of MIMEBase and named as p
     p = MIMEBase('application', 'octet-stream')
-    # To change the payload into encoded form
+                                                                                               # To change the payload into encoded form
     p.set_payload((attachment).read())
 
 
-    # encode into base64
+                                                                                               # encode into base64
     encoders.encode_base64(p)
 
     p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
-    # attach the instance 'p' to instance 'msg'
+                                                                                               # attach the instance 'p' to instance 'msg'
     msg.attach(p)
 
-    # creates SMTP session
+                                                                                               # creates SMTP session
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(fromaddr,password)
 
-    # Converts the Multipart msg into a string
+                                                                                                # Converts the Multipart msg into a string
     text = msg.as_string()
 
     server.send_message(msg)
